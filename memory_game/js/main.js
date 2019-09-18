69506f7656fa3e35ded59f0161f699f7904a8556
kbasha1 <kbaska1978@gmail.com>
2019-09-18 11:21:29 +0300

    creat game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,29 @@ console.log("User flipped queen");
 console.log("User flipped king");
 console.log("Sorry, try again");
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+
+];
 
 const cardsInPlay = [];
 function checkForMatch() {
@@ -17,8 +39,10 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 
 
 function flipCard (cardId) {
-	checkForMatch ();
-	console.log("User flipped " + cards[cardId]);
+	checkForMatch (); {
+	console.log("User flipped " + cards[cardId].rank);
+};
+
 	var cardOne = cards[0];
 
 cardsInPlay.push("cardOne");
@@ -28,9 +52,9 @@ var cardTwo = cards[2];
 
 cardsInPlay.push("cardTwo");
 console.log("User flipped " + cardTwo);
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 }
 
 flipCard(0);
 flipCard(2);
-}
+};
